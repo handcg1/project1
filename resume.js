dcd99e19a20d2ccb5f46ecbfe526c2d8ef73add2
handcg1 <handcg@dukes.jmu.edu>
2020-10-01 11:02:52 -0400

    resume page changes

--- resume.js
+++ resume.js
@@ -1,15 +1,30 @@
 const arrow = document.getElementById("arrow");
 const slide = document.getElementById("card");
 const arrow2 = document.getElementById("arrow2");
+const slide2 = document.getElementById("card2");
 
 slide.classList.add('hidden');
 
 arrow.addEventListener('click', () => {
-    slide.classList.remove('hidden');
-    slide.classList.add('visible');
+    if(slide.classList.contains('hidden')) {
+        slide.classList.remove('hidden');
+        slide.classList.add('visible');
+        arrow.style.transform = "rotate(180deg) scale(1.5)";
+    } else {
+        slide.classList.remove('visible');
+        slide.classList.add('hidden');
+        arrow.style.transform = "rotate(360deg) scale(1.5)";
+    }  
 });
 
 arrow2.addEventListener('click', () => {
-    slide.classList.remove('visible');
-    slide.classList.add('hidden');
+    if(slide.classList.contains('hidden')) {
+        slide2.classList.remove('hidden');
+        slide2.classList.add('visible');
+        arrow2.style.transform = "rotate(180deg) scale(1.5)";
+    } else {
+        slide2.classList.remove('visible');
+        slide2.classList.add('hidden');
+        arrow2.style.transform = "rotate(360deg) scale(1.5)";
+    }  
 });
