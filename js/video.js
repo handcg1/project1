fc74fbcc2b35b7975cef174ae3fe0127aaf10399
handcg1 <handcg@dukes.jmu.edu>
2020-10-09 11:28:03 -0400

    added animation

--- js/video.js
+++ js/video.js
@@ -1,24 +1,63 @@
-var slideIndex = 1;
+const slides = document.querySelectorAll('.myslides');
+const dots = document.querySelectorAll('.dot');
 
-showDivs(slideIndex);
+dots[0].classList.add('active');
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+let i = 0;
+
+function forward() {
+  if (i < slides.length - 1) {
+    slides[i].classList.remove('current');
+    slides[i].classList.add('past');
+    dots[i].classList.remove('active');
+    i += 1;
+    slides[i].classList.add('current');
+    dots[i].classList.add('active');
+
+  }
 }
 
-function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("myslides");
-    if (n > x.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = x.length;
-    }
+function backward() {
+ 
+  if (i > 0) {
+    slides[i].classList.remove('current');
+    dots[i].classList.remove('active');
+    i -= 1;
+    dots[i].classList.add('active');
+    slides[i].classList.remove('past');
+    slides[i].classList.add('current');
+  } 
 
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-
-    x[slideIndex-1].style.display = "block";
 }
+
+document.getElementById('left').addEventListener('click', backward);
+document.getElementById('right').addEventListener('click', forward);
+
+
+
+
+
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//     showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//     var i;
+//     var x = document.getElementsByClassName("myslides");
+//     if (n > x.length) {
+//         slideIndex = 1;
+//     }
+//     if (n < 1) {
+//         slideIndex = x.length;
+//     }
+
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none";
+//     }
+
+//     x[slideIndex-1].style.display = "block";
+//     x[slideIndex-1]
+// }
