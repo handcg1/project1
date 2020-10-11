937bd3ce8516c6d291f0ebc67545c652743f94d4
handcg1 <handcg@dukes.jmu.edu>
2020-10-11 16:30:10 -0400

    add form data collection

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener('click', () => {
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+
+
+    window.open(`mailto:johnrhand4@gmail.com?subject=${subject}&body=${message}`);
+
+}); 
