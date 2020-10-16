2c011d07f48274d33072ac9cce0fa7eff0715b0f
handcg1 <handcg@dukes.jmu.edu>
2020-10-15 23:54:19 -0400

    clean up

--- js/video.js
+++ js/video.js
@@ -32,32 +32,3 @@ function backward() {
 
 document.getElementById('left').addEventListener('click', backward);
 document.getElementById('right').addEventListener('click', forward);
-
-
-
-
-
-// var slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//     showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//     var i;
-//     var x = document.getElementsByClassName("myslides");
-//     if (n > x.length) {
-//         slideIndex = 1;
-//     }
-//     if (n < 1) {
-//         slideIndex = x.length;
-//     }
-
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none";
-//     }
-
-//     x[slideIndex-1].style.display = "block";
-//     x[slideIndex-1]
-// }
